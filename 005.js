44bbf2b1d554e074c0c90bd36b3e86b8910f1900
Anna Moskalchuk <anna@crystax.net>
2017-10-13 16:43:05 +0300

    learning JS

---
+++ 005.js
@@ -0,0 +1,25 @@
+$ (function() {
+  $("#show").html("Show")
+});
+
+/*var dialog = document.querySelector('dialog');
+  document.querySelector('#show').onclick = function() {
+    dialog.showModal();
+  }
+
+document.querySelector('#close').onclick = function() {
+  dialog.close();
+}
+
+document.querySelector('#Ok').onclick = function() {
+  var txt = document.getElementById("name").value;
+  dialog.close();
+  alert(txt);
+}
+
+//Validator of form
+
+
+
+
+
