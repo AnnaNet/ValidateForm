c3500006f7576de2f8e9144f19e10911d1903d86
Anna Moskalchuk <anna@crystax.net>
2017-10-14 13:06:08 +0300

    Working!

--- 005.js
+++ 005.js
@@ -2,6 +2,9 @@
 document.querySelector('#Ok').onclick = function() {
   document.getElementById("Error1").innerHTML = "";
   document.getElementById("Error2").innerHTML = "";
+  document.getElementById("Error3").innerHTML = "";
+  document.getElementById("Error4").innerHTML = "";
+
   if (document.getElementById("name").value == "") {
     document.getElementById("Error1").innerHTML = "Укажите от кого";
   }
@@ -12,6 +15,11 @@ document.querySelector('#Ok').onclick = function() {
   document.getElementById("password").value) {
     document.getElementById("Error2").innerHTML = "Пароли не совпадают";
   }
-
+  if (document.getElementById("place").value == 0) {
+    document.getElementById("Error3").innerHTML = "Укажите, куда";
+  }
+  if (!document.getElementById("area").value) {
+    document.getElementById("Error4").innerHTML = "Отсутствует текст";
+  }
 }
   //Validator of form
