5b9346b7c0ba852bca4ab3e547b14aa74eaa14d4
Anna Moskalchuk <anna@crystax.net>
2017-10-18 22:36:27 +0300

    validate form jQuery

--- 005.js
+++ 005.js
@@ -1,4 +1,4 @@
-
+/*
 document.querySelector('#Ok').onclick = function() {
   document.getElementById("Error1").innerHTML = "";
   document.getElementById("Error2").innerHTML = "";
@@ -22,4 +22,36 @@ document.querySelector('#Ok').onclick = function() {
     document.getElementById("Error4").innerHTML = "Отсутствует текст";
   }
 }
-  //Validator of form
+  //Validator of form (Brouser API)
+
+*/
+
+//Validator of form jQuery
+
+document.querySelector('#Ok').onclick = function() {
+
+  $("#Error1").text("");
+  $("#Error2").text("");
+  $("#Error3").text("");
+  $("#Error4").text("");
+
+  if ($("#name").val() == "") {
+    $("#Error1").text("Укажите от кого");
+  }
+
+  if ($("#password").val() == "") {
+    $("#Error2").text("Укажите пароль");
+  }
+
+  if ($("#password").val() != $("#passwordRep").val()) {
+    $("#Error2").text("Пароли не совпадают");
+  }
+
+  if ($("#place").val() == 0) {
+    $("#Error3").text("Укажите, куда");
+  }
+
+  if (!$("#area").val()) {
+    $("#Error4").text("Отсутствует текст");
+  }
+}
